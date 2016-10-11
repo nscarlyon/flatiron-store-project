382c6edbf10ed8458fcb4fbf605a1c78c695e940
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 18:59:15 -0400

    add item model logic

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  has_many :line_items
+
+  def self.available_items
+    available_items = []
+    Item.all.map do |i|
+      if i.inventory > 0
+        available_items << i.title
+      end
+    end
+    available_items
+  end
 end
