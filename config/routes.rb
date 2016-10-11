b72d342856dd6d9d0bf83b246cc01a93d8f049c9
nscarlyon <natasha.carlyon@gmail.com>
2016-10-10 23:02:34 -0400

    add bootstrap store theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'store/index'
-
-    root 'store#index', as: 'store'
+  root 'store#index', as: 'store'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
