059f5eb87570e702dfce842213aad700fd47c3f7
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 18:33:38 -0400

    add login button on signup form

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
     var login = document.getElementById('login');
     var signupBtn = document.getElementById('signupBtn');
     var signup = document.getElementById('signup');
+    var loginBtn = document.getElementById('loginBtn');
 
     window.onclick = function() {
         if (event.target == login) {
+            login.style.display = "none";
+        }
+        if (event.target == loginBtn) {
             login.style.display = "none";
         }
         if (event.target == signupBtn) {
