7227b48441bf10015f9f198050269253ba68b99d
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 19:14:41 -0400

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
+counter = 1
 10.times do
   Item.create(
     title: Faker::Commerce.product_name,
     inventory: Faker::Number.number(2),
-    price: Faker::Number.number(4)
+    price: Faker::Number.number(4),
+    category_id: counter
   )
-  Category.create(title: Faker::Commerce.department)
-end
-
-User.create!(email: "test@email.com", password: "password")
-
-counter = 1
-Item.all.each do |item|
-  item.category_id = counter
-  item.save
-  counter += 1
+  counter+=1
 end
