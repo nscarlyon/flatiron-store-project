7ed0697e9af26d22072d77b51a278e3f7483ba93
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 17:53:42 -0400

    when user logs out, user's name dissapears

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'item/show'
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
   root 'store#index', as: 'store'
   resources :item
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
