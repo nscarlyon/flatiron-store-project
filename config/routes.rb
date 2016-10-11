2a299b1f66cc826c225e77924f249cde4a60632d
nscarlyon <natasha.carlyon@gmail.com>
2016-10-10 23:11:06 -0400

    add bootstrap item theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'item/show'
+
   devise_for :users
   root 'store#index', as: 'store'
+  resources :item
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
