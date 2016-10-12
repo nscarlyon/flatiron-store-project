6b2528e0f6555b2004d60d11516036b360006612
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 20:14:33 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'cart/show'
-
-  get 'item/show'
-
   devise_for :users
   root 'store#index', as: 'store'
-  resources :item
+  resources :items
   resources :carts
+  resources :line_items
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
