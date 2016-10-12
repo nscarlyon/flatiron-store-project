a48dc7bcb8ffaba5404a6ed8e5f6937cadbd495d
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 20:50:01 -0400

    fix line_items create action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,12 +2,16 @@ class LineItemsController < ApplicationController
 
   def create
     item = Item.find(params[:item_id])
-
-    if current_user.current_cart.nil?
-      current_user.current_cart = Cart.new
+    
+    if current_user.current_cart_id.nil?
+      cart = Cart.create(user_id: current_user.id)
+      current_user.current_cart_id = cart.id
+      current_user.save
     end
 
+    current_user.current_cart = Cart.find(current_user.current_cart_id)
     current_user.current_cart.add_item(item.id)
     current_user.save
-    redirect_to cart_path(current_user.current_cart)
+    redirect_to cart_path(current_user.current_cart_id)
+  end
 end
