25a4fa27f620b8106f8060ac9ebe4d05fbeb7042
nscarlyon <natasha.carlyon@gmail.com>
2016-10-11 20:54:35 -0400

    add checkout action

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :carts
   resources :line_items
   resources :orders
+  post 'carts/:id', to: 'carts#checkout', as: 'checkout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
