4d800e6186768974c05204a79f41ec331828badc
nscarlyon <natasha.carlyon@gmail.com>
2016-10-13 15:21:51 -0400

    modify carts checkout action to accomodate removed current_cart_id column

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,11 +4,10 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    @current_cart = current_user.current_cart
+    @current_cart = Cart.find_by(user_id: current_user.id)
     @current_cart.new_inventory
 
     @current_cart.destroy
-    current_user.current_cart_id = nil
     redirect_to cart_path(@current_cart)
   end
 end
