2129247cdce96e6cbd939933488cf7520c213c3d
nscarlyon <natasha.carlyon@gmail.com>
2016-10-20 13:47:04 -0400

    user can select # of stars for item

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,9 +1,16 @@
-
-
 $(document).ready(function(){
-  document.getElementById('')
 })
 
 toggleStar = function(event) {
-  alert("test!")
+  event.target.className == "fa fa-star-o" ? fillStar(event) : emptyStar(event)
+}
+
+fillStar = function(event) {
+  event.target.className = "fa fa-star";
+  review_rating.value =  parseInt($('#review_rating')[0].value) + 1;
+}
+
+emptyStar = function(event) {
+  event.target.className = "fa fa-star-o";
+  review_rating.value = parseInt($('#review_rating')[0].value) - 1;
 }
