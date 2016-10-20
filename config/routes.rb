f785936be46096632d492fc30ddaf9bbdcafc21a
nscarlyon <natasha.carlyon@gmail.com>
2016-10-20 10:34:17 -0400

    add nested reviews route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :path=> '', controllers: { sessions: 'users/sessions', :registrations=>'users/registrations'}
   root 'store#index', as: 'store'
-  resources :items
   resources :carts
   resources :line_items
   resources :orders
   post 'carts/:id', to: 'carts#checkout', as: 'checkout'
+
+  resources :items do
+    resources :reviews
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
