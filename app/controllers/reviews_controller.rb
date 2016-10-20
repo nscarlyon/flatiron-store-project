0d81ae9315b89d28f4c4755a77e8e038b88ce3fd
nscarlyon <natasha.carlyon@gmail.com>
2016-10-20 11:21:26 -0400

    add create action and review_params method to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,15 @@ class ReviewsController < ApplicationController
 
   def show
   end
+
+  def create
+    @review = Review.create(review_params)
+    redirect_to item_path(@review.item.id)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :item_id, :comment, :rating)
+  end
 end
