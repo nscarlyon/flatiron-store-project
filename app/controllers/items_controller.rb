1952539c58d320bf825e170d2f9683d5d655651a
nscarlyon <natasha.carlyon@gmail.com>
2016-10-21 18:03:57 -0400

    item show responds to json and html

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,10 @@ class ItemsController < ApplicationController
     @item = Item.find_by(id: params[:id])
     @review = Review.new
     @reviews = @item.reviews
+
+    respond_to do |f|
+      f.html {render :show}
+      f.json {render json: @item}
+    end
   end
 end
